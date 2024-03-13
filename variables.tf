3acace1480fd8ab64fcaf4736f58ce5a7516b5e7
pavbjj <43717447+pavbjj@users.noreply.github.com>
2024-03-13 15:43:20 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "api_url" {
+  default = "https://<your-tenant>.console.ves.volterra.io/api"
+}
+
+variable "api_p12_file" {
+}
+
+variable "base" {
+  default = "tf-csv"
+}
+
+variable "namespace" {
+
+}
